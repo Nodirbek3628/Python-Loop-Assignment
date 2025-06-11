c95e7f28c7a93c75a9060be4e53ceef01c6f85ff
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-06-11 20:28:14 +0500

    Bajarildi

--- task09.py
+++ task09.py
@@ -1,20 +1,24 @@
-import random                                #PIN kod ochish o‘yini
-random = random.randint(1000,9999)
-print(random)
-count = 0                          #Kompyuter 4 xonali maxfiy raqam (PIN) yaratadi.
-                                   # Foydalanuvchi bu raqamni topishga harakat qiladi. Har urinishda:
+import random    
+                                           
+pin = random.randint(1000,9999)
+
+count = 0 
+print("4 xonalik maxfiy raqam yaratadi")
+print("7 urinish topib ko'ring")                        
+
 while count < 8:
     option = int(input("maxfiy raqamni kiriting; "))
     count += 1
-    if option > random:
+
+    if option > pin:
         print("Juda katta son:")
-        break
-    elif option == random:
+
+    elif option == pin:
         print("Tabriklaymz:")
         break
     else:
         print("Juda kichik son:")
-        break
 
 else:
-    print("Urinishlar soni tugadi",random)
+    print("afsus topolmadz")
+    print(f"To'g'ri javob {pin}")
